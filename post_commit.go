82575c3c7b8d99538ad2b578f86ab8484782aae0
Sam Boyer <tech@samboyer.org>
2015-11-06 00:55:19 -0500

    Copy over pvgit files

---
+++ post_commit.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pipeviz/pvgit/Godeps/_workspace/src/github.com/pipeviz/pipeviz/ingest"
+	"github.com/pipeviz/pvgit/Godeps/_workspace/src/github.com/spf13/cobra"
+)
+
+func postCommitHookCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "hook-post-commit",
+		Short: "Processes a git post-commit hook event.",
+		Run:   runPostCommit,
+	}
+
+	return cmd
+}
+
+func runPostCommit(cmd *cobra.Command, args []string) {
+	repo := getRepoOrExit()
+
+	head, err := repo.Head()
+	if err != nil {
+		log.Fatalf("Could not get repo HEAD")
+	}
+
+	commit, err := repo.LookupCommit(head.Target())
+	if err != nil {
+		log.Fatalf("Could not find commit pointed at by HEAD")
+	}
+
+	ident, err := GetRepoIdent(repo)
+	if err != nil {
+		log.Fatalln("Failed to retrieve identifier for repository")
+	}
+
+	m := new(ingest.Message)
+	m.Add(commitToSemanticForm(commit, ident))
+
+	recordHead(m, repo)
+	sendMapToPipeviz(m, repo)
+}
---
+++ pvgit.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/pipeviz/pvgit/Godeps/_workspace/src/github.com/spf13/cobra"
+
+func main() {
+	root := &cobra.Command{Use: "pvgit"}
+	root.AddCommand(postCommitHookCommand())
+	root.AddCommand(postCheckoutHookCommand())
+	root.AddCommand(syncCommand())
+	root.AddCommand(instrumentCommand())
+
+	root.Execute()
+}
